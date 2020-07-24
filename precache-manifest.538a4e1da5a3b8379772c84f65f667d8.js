677b59f632bd14282dca8a647db030118ebb9641
Osvaldo Gioia <osvaldogioia@hotmail.com>
2020-07-24 19:25:49 -0300

    Add files via upload

---
+++ precache-manifest.538a4e1da5a3b8379772c84f65f667d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d01145231f4fbc79c81fe0bc6ec065",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13f25fc0682736b0618a",
+    "url": "/static/css/main.a7ffdfe3.chunk.css"
+  },
+  {
+    "revision": "7948e92bb08a9a2a8bab",
+    "url": "/static/js/2.f7854317.chunk.js"
+  },
+  {
+    "revision": "8fcb6aaf549782e0d7cb66ea3ac5c5f2",
+    "url": "/static/js/2.f7854317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f25fc0682736b0618a",
+    "url": "/static/js/main.c124dd83.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
