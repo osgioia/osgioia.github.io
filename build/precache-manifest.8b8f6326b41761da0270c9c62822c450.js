1c80d0dd27812435b76ad399155701ab3e19997c
Osvaldo Gioia <os.gioia@gmail.com>
2024-07-13 22:52:04 -0400

    add cname

---
+++ build/precache-manifest.8b8f6326b41761da0270c9c62822c450.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29fd97700189335a4ffcd379851e45ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "43a061eb1e6a4e6fc73d",
+    "url": "./static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "41f7a6785c2718cc831c",
+    "url": "./static/css/main.98e576d8.chunk.css"
+  },
+  {
+    "revision": "43a061eb1e6a4e6fc73d",
+    "url": "./static/js/2.f7bbc722.chunk.js"
+  },
+  {
+    "revision": "484b3a90b75ec38dbb4ca2cd8fcfc3fd",
+    "url": "./static/js/2.f7bbc722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f7a6785c2718cc831c",
+    "url": "./static/js/main.77bfab54.chunk.js"
+  },
+  {
+    "revision": "9c8a17d9b97f24657c24",
+    "url": "./static/js/runtime-main.112b14dd.js"
+  },
+  {
+    "revision": "a90c261fd9f57321bff3c28426715450",
+    "url": "./static/media/profile.a90c261f.png"
+  }
+]);
