5fd067cf484d983ab19060be771f68a71e259140
Osvaldo Gioia <os.gioia@gmail.com>
2024-07-13 21:52:34 -0400

    CV added

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
       <p className="text-gray-600 mb-4">
         Made with <span role="img" aria-label="heart">💙</span>
       </p>
-      <a className="text-blue-500 hover:underline" href={`cv/resume.pdf`} download>Download my resume</a>
+      <a className="text-blue-500 hover:underline" href={`https://drive.google.com/uc?export=download&id=1KanUgPUhFp92zqyugFGoHv0ABEb4ahoH`} download>Download my resume</a>
     </div>
   )
 }
