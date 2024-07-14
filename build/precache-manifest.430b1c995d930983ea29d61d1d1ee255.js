7d0603eac2a6b5c568a705b8762b34dc5ffd51cd
Osvaldo Gioia <os.gioia@gmail.com>
2024-07-13 22:02:42 -0400

    git ignore

---
+++ build/precache-manifest.430b1c995d930983ea29d61d1d1ee255.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47f7058ca944bc4769340b25dc13932",
+    "url": "/osgioia.dev/index.html"
+  },
+  {
+    "revision": "43a061eb1e6a4e6fc73d",
+    "url": "/osgioia.dev/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "41f7a6785c2718cc831c",
+    "url": "/osgioia.dev/static/css/main.98e576d8.chunk.css"
+  },
+  {
+    "revision": "43a061eb1e6a4e6fc73d",
+    "url": "/osgioia.dev/static/js/2.f7bbc722.chunk.js"
+  },
+  {
+    "revision": "484b3a90b75ec38dbb4ca2cd8fcfc3fd",
+    "url": "/osgioia.dev/static/js/2.f7bbc722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f7a6785c2718cc831c",
+    "url": "/osgioia.dev/static/js/main.77bfab54.chunk.js"
+  },
+  {
+    "revision": "75936f874ef4c1957fdf",
+    "url": "/osgioia.dev/static/js/runtime-main.4f93d3c9.js"
+  },
+  {
+    "revision": "a90c261fd9f57321bff3c28426715450",
+    "url": "/osgioia.dev/static/media/profile.a90c261f.png"
+  }
+]);
