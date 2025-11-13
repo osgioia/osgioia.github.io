d2734aa60a3f5394d02a10e3e9f4a561dd663bef
Osvaldo Gioia <os.gioia@gmail.com>
2025-11-12 23:19:29 -0300

    chore: update react and dependencies versions

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  darkMode: 'class', // Enable dark mode with class strategy
+  theme: {
+    extend: {
+      // You can extend the default theme here if needed
+    },
+  },
+  plugins: [],
+}
+
